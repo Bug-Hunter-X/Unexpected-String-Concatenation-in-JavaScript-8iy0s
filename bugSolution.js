97b97d9b6b7409d97707b83c400c49e7d9d9512b
BugHunter-X <askzenai@gmail.com>
2024-12-14 03:38:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,1 @@
+function foo(a,b){if(typeof a === 'number' && typeof b === 'number'){return a + b;}else{return "Error: Inputs must be numbers";} }console.log(foo(1,2)); // Output: 3console.log(foo(1,"2")); // Output: Error: Inputs must be numbers
